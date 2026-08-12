0ff8f83fa0fb302d51231dbdfea97d0089581d3c
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-08-11 19:01:46 -0500

    Creacion de proyecto de clases y 2 clases employee y user con exten

---
+++ PaymentDiscoubtApp/src/main/java/domain/Employee.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.Scanner;
+
+public class Employee extends User{
+    public Scanner sc = new Scanner(System.in);
+
+    //Atributes
+
+    private String role;
+    private String area;
+
+
+    public void create(){
+
+    }
+
+    public void select(){
+
+    }
+}
---
+++ PaymentDiscoubtApp/src/main/java/domain/User.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.Scanner;
+
+public class User {
+    public Scanner sc = new Scanner(System.in);
+
+    //Atributes
+
+    private int id;
+    private String name;
+    private String lasName;
+    private String email;
+    private String phone;
+    private String status;
+
+    public void create(){
+
+    }
+
+    public void select(){
+
+    }
+
+}
